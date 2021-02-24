309d4ef4e0bb59e4f9a3f6dd685e3f8881076901
JumanaAmr <jumana.amr@ocsbstudent.ca>
2021-02-24 00:29:39 +0000

    Trial 4

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from os import system, name
-def func1() :
 
-  
-  d=input("Enter diameter:")
-  d = float (d)
-  a = 1 / 4 * 3.14 * d * d
-  c = 3.14 * d
-  print('Area =', a)
-  print('Circumference =', c)
-E = input("Press y  to enter another diameter?:")
+
+def func1(): 
+    d = input("Enter diameter:")
+    d = float(d)
+    a = 1 / 4 * 3.14 * d * d
+    c = 3.14 * d
+    print("Area =", a)
+    print("Circumference =", c)
+    func1()
+
+
+E = input("Press Y  to enter another diameter?:")
 E = str(E)
-while E=='y':
-    _ = system('clear')
+while E == "Y":
+    _ = system("clear")
     func1()
     E=input("Press Y  to enter another diameter?:")
     E=str(E)
